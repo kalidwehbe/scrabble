e539a7c8f3d9415c1fefedfeb5e3f8d9686b8d3d
kalidwehbe <157623205+kalidwehbe@users.noreply.github.com>
2025-11-26 00:37:53 -0500

    Create GameState.java

---
+++ GameState.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ * Represents a snapshot of the game state for Undo/Redo.
+ */
+public class GameState {
+    public Board board;
+    public List<Player> players;
+    public Queue<Tile> bag;
+    public int currentPlayerIndex;
+    public boolean firstMove;
+
+    public GameState(Board board, List<Player> players, Queue<Tile> bag, int currentPlayerIndex, boolean firstMove) {
+        this.board = board;
+        this.players = players;
+        this.bag = bag;
+        this.currentPlayerIndex = currentPlayerIndex;
+        this.firstMove = firstMove;
+    }
+}
